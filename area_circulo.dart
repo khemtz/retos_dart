47197455c7d3a52ebfef5d886a8a37b7cf9dc090
Manuel Franco Gutiérrez <manufranco96@gmail.com>
2022-02-26 11:41:30 -0600

    se agrego formula para menu, triangulo y cuadrado

--- area_circulo.dart
+++ area_circulo.dart
@@ -6,8 +6,18 @@ void AreaCirculo(int r) {
   print("el area es:" + area.toString());
 }
 
+void AreaTriangulo(int b, int a) {
+  var area = (b * a) / 2;
+  print("el area es:" + area.toString());
+}
+
 void AreaRectangulo(int b, int a) {
   var area = b * a;
+  print("el area es:" + area.toString());
+}
+
+void AreaCuadrado(int l) {
+  var area = l * l;
   print("el area es:" + area.toString());
 }
 
@@ -18,16 +28,19 @@ int LeerConsolaint(String msje) {
   return read;
 }
 
+void imprimeMenu() {
+  print("\n\n******* MENU ********");
+  print("1.- Circulo");
+  print("2.- Rectangulo");
+  print("3.- Triangulo");
+  print("4.- Cuadrado");
+  print("0.- Salir");
+}
+
 void main() {
   var respuesta = 9;
-
+  imprimeMenu();
   while (respuesta != 0) {
-    print("\n\n******* MENU ********");
-    print("1.- Circulo");
-    print("2.- Rectangulo");
-    print("3.- Triangulo");
-    print("4.- Cuadrado");
-    print("0.- Salir");
     respuesta = LeerConsolaint("Elige una opcion:");
 
     if (respuesta == 1) {
@@ -39,8 +52,14 @@ void main() {
       int altura = LeerConsolaint("cual es la altura:");
       AreaRectangulo(base, altura);
     } else if (respuesta == 3) {
-      print("calculo triangulo");
+      //print("calculo triangulo");
+      int base = LeerConsolaint("cual es la base:");
+      int altura = LeerConsolaint("cual es la altura:");
+      AreaTriangulo(base, altura);
+    } else if (respuesta == 4) {
+      int lado = LeerConsolaint("cual es el lado:");
+      AreaCuadrado(lado);
     }
+    //print(radio.toString());
   }
-  //print(radio.toString());
 }
